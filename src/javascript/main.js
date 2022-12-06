88e48e4d56e5d62ddc693aef51949602e9047628
ROXBOZ <hello@roxanne-borloz.net>
2022-12-06 17:33:47 +0100

    fetched API title and images, added modal

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -4,50 +4,101 @@ function buildGrid() {
   const row = document.createElement("div");
   row.setAttribute("class", "row");
 
-  for (i = 0; i < 200; i++) {
+  for (i = 0; i < soups.length; i++) {
     // /* Create Columns */
     const col = document.createElement("div");
     col.setAttribute("class", "col-sm-12 col-md-6 col-lg-4 pb-4");
 
-    // /* Create Cards */
+    /* Create Recipe Container */
+    const recipeContainer = document.createElement("div");
+    recipeContainer.setAttribute("class", "recipe-container");
+
+    /* Create Cards */
     const card = document.createElement("div");
     card.setAttribute("class", "card border-primary");
 
-    // /* Create Card image */
+    /* Create Card image */
     const img = document.createElement("img");
     img.setAttribute("class", "card-img-top");
-    img.src =
-      "https://hips.hearstapps.com/hmg-prod/images/homemade-pumpkin-soup-royalty-free-image-1571855802.jpg?crop=1.00xw:0.756xh;0,0.112xh&resize=1200:*";
-    img.alt = "Card image cap";
+    // img.src = soups[i]["image"];
+    img.src = "assets/images/soup.png";
+    img.setAttribute("style", "padding: 1rem");
+    img.alt = soups[i]["title"];
 
-    // /* Create Card Body */
+    /* Create Card Body */
     const cardBody = document.createElement("div");
     cardBody.setAttribute("class", "card-body");
 
-    // /* Create Card Title */
+    /* Create Card Title */
     const cardTitle = document.createElement("h2");
-    cardTitle.innerText = "Card title";
+    cardTitle.innerText = soups[i]["title"];
 
     // /* Create Card Text */
-    const cardText = document.createElement("p");
-    cardText.innerText = "Lorem";
+    // const cardText = document.createElement("p");
+    // cardText.innerText = "Lorem";
 
-    // /* Create Card Link */
-    const cardLink = document.createElement("a");
-    cardLink.href = "#";
-    cardLink.setAttribute("class", "btn btn-primary");
-    cardLink.innerText = "Go somewhere";
+    /* Create Card Link */
+    const cardButton = document.createElement("button");
+    cardButton.setAttribute("id", "recipeOpen");
+    cardButton.setAttribute("class", "btn btn-primary");
+    cardButton.innerText = "See Recipe";
+    cardButton.addEventListener("click", () => {
+      cardModal.style.display = "block";
+    });
+
+    /* Create Modal */
+    const cardModal = document.createElement("div");
+    cardModal.setAttribute("id", "recipeModal");
+    cardModal.setAttribute("class", "modal p-5 mx-auto");
+    cardModal.style.display = "none";
+    cardModal.style.position = "fixed";
+    cardModal.style.zIndex = "1";
+    cardModal.style.overflow = "auto";
+    cardModal.style.width = "100%";
+    cardModal.style.height = "100%";
+    cardModal.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+
+    const modalContent = document.createElement("div");
+    modalContent.setAttribute(
+      "class",
+      "modal-content bg-white p-2 w-25 border-primary mx-auto"
+    );
+    modalContent.style.display = "grid";
+
+    const modalClose = document.createElement("span");
+    modalClose.setAttribute("class", "close text-primary");
+    modalClose.style.fontSize = "200%";
+    modalClose.style.justifySelf = "flex-end";
+    modalClose.style.fontWeight = "bold";
+    modalClose.innerHTML = "&times;";
+    modalClose.addEventListener("click", () => {
+      cardModal.style.display = "none";
+    });
+
+    const modalText = document.createElement("p");
+    modalText.innerText = "some text for now";
 
     /* Building Grid */
     container.append(row);
     row.append(col);
-    col.append(card);
+    col.append(recipeContainer);
+    recipeContainer.append(card);
     card.append(img);
     card.append(cardBody);
     cardBody.append(cardTitle);
-    cardBody.append(cardText);
-    cardBody.append(cardLink);
+    // cardBody.append(cardText);
+    cardBody.append(cardButton);
+    recipeContainer.append(cardModal);
+    cardModal.append(modalContent);
+    modalContent.append(modalClose);
+    modalContent.append(modalText);
+
+    const body = document.querySelector("body");
+    body.addEventListener("click", (event) => {
+      if (event.target == cardModal) {
+        cardModal.style.display = "none";
+      }
+    });
   }
 }
-
 buildGrid();

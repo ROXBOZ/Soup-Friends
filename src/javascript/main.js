5bc0da9841958520478123d054a4155be6c78586
ROXBOZ <hello@roxanne-borloz.net>
2022-12-07 18:06:10 +0100

    modal recipe, icons, more api struggle

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -15,6 +15,8 @@ function showMore() {
   }
 }
 
+/* Build Grid */
+
 function buildGrid() {
   const container = document.getElementById("soup-container");
   /* Create Rows */
@@ -50,10 +52,6 @@ function buildGrid() {
     const cardTitle = document.createElement("h2");
     cardTitle.innerText = soups[i]["title"];
 
-    // /* Create Card Text */
-    // const cardText = document.createElement("p");
-    // cardText.innerText = "Lorem";
-
     /* Create Card Link */
     const cardButton = document.createElement("button");
     cardButton.setAttribute("id", "recipeOpen");
@@ -65,8 +63,9 @@ function buildGrid() {
 
     /* Create Modal */
     const cardModal = document.createElement("div");
-    cardModal.setAttribute("id", "recipeModal");
+    cardModal.setAttribute("id", "cardModal"); // modal screen with black background
     cardModal.setAttribute("class", "modal p-5 mx-auto");
+    /* Modal Styling */
     cardModal.style.display = "none";
     cardModal.style.position = "fixed";
     cardModal.style.zIndex = "1";
@@ -75,27 +74,103 @@ function buildGrid() {
     cardModal.style.height = "100%";
     cardModal.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
 
-    const modalContent = document.createElement("div");
+    const modalContent = document.createElement("div"); // modal white box
     modalContent.setAttribute(
       "class",
-      "modal-content bg-white p-2 w-25 border-primary mx-auto"
+      "modal-content bg-white w-75 p-2 border-primary mx-auto"
     );
     modalContent.style.display = "grid";
 
-    const modalClose = document.createElement("span");
-    modalClose.setAttribute("class", "close text-primary");
+    // Modal Close Button
+    const modalClose = document.createElement("i");
+    modalClose.setAttribute(
+      "class",
+      "close text-primary fa-solid fa-xmark m-3"
+    );
     modalClose.style.fontSize = "200%";
     modalClose.style.justifySelf = "flex-end";
     modalClose.style.fontWeight = "bold";
-    modalClose.innerHTML = "&times;";
     modalClose.addEventListener("click", () => {
       cardModal.style.display = "none";
     });
 
-    const modalText = document.createElement("p");
-    modalText.innerText = "some text for now";
+    /* Recipe inside modal */
+    const modalText = document.createElement("div");
+    modalText.setAttribute("class", "recipe px-5");
+    const modalTitle = document.createElement("h2");
+    modalTitle.setAttribute("class", "mb-5");
+    modalTitle.innerText = soups[i]["title"]; // Repetition
+    const recipeIng = document.createElement("div");
+    recipeIng.setAttribute("class", "ingredients");
+    const ingList = document.createElement("ul");
+    const extIng = cucumber["extendedIngredients"]; // Replace cucumber
 
-    /* Building Grid */
+    /* Listing Ingredients */
+    for (i = 0; i < extIng.length; i++) {
+      const ingName = extIng[i]["name"];
+      const ingListItem = document.createElement("li");
+      ingListItem.innerText = ingName;
+      ingList.append(ingListItem);
+    }
+
+    /* Icons */
+
+    //time
+    const iconsContainer = document.createElement("p");
+    iconsContainer.setAttribute("class", "icons-container");
+    const timeContainer = document.createElement("span");
+    timeContainer.setAttribute("class", "time-container mr-5");
+    const alarmIcon = document.createElement("i");
+    alarmIcon.setAttribute("class", "fa-solid fa-bell m-1");
+    const recipeTime = `${cucumber["readyInMinutes"]} min.`;
+
+    //vegan
+    const isItVegan = cucumber["vegan"];
+    const veganContainer = document.createElement("span");
+    veganContainer.setAttribute("class", "vegan-container");
+    if (isItVegan === true) {
+      const itIsVeganText = "vegan";
+      const itIsVegan = document.createElement("i");
+      itIsVegan.setAttribute("class", "fa-solid fa-leaf m-1");
+      veganContainer.append(itIsVegan);
+      veganContainer.append(itIsVeganText);
+    } else {
+      const itIsNotVeganText = "not vegan";
+      const itIsNotVegan = document.createElement("i");
+      itIsNotVegan.setAttribute("class", "fa-solid fa-skull m-1");
+      veganContainer.append(itIsNotVegan);
+      veganContainer.append(itIsNotVeganText);
+    }
+
+    //gluten
+    const isItglutenFree = cucumber["glutenFree"];
+    const glutenContainer = document.createElement("span");
+    if (isItglutenFree === true) {
+      const itIsglutenFreeText = "gluten free";
+      const itIsglutenFree = document.createElement("i");
+      itIsglutenFree.setAttribute("class", "fa-solid fa-bowl-rice m-1");
+      glutenContainer.append(itIsglutenFree);
+      glutenContainer.append(itIsglutenFreeText);
+    } else {
+      const itIsNotglutenFreeText = "not vegan";
+      const itIsNotglutenFree = document.createElement("i");
+      itIsNotglutenFree.setAttribute("class", "fa-solid fa-wheat m-1");
+      glutenContainer.append(itIsNotglutenFree);
+      glutenContainer.append(itIsNotglutenFreeText);
+    }
+
+    /* How to */
+    const instructionText = document.createElement("p");
+    instructionText.setAttribute("class", "instruction-text");
+    const recipeInstruction = cucumber["instructions"];
+    if (recipeInstruction === true) {
+      instructionText.append(recipeInstruction);
+    } else {
+      instructionText.innerText =
+        "There is no instructions for this recipe. What could go wrong anyways?";
+    }
+
+    /* Generator */
     container.append(row);
     row.append(col);
     col.append(recipeContainer);
@@ -103,12 +178,21 @@ function buildGrid() {
     card.append(img);
     card.append(cardBody);
     cardBody.append(cardTitle);
-    // cardBody.append(cardText);
     cardBody.append(cardButton);
     recipeContainer.append(cardModal);
     cardModal.append(modalContent);
     modalContent.append(modalClose);
     modalContent.append(modalText);
+    modalText.append(modalTitle);
+    modalText.append(recipeIng);
+    recipeIng.append(ingList);
+    modalText.append(iconsContainer);
+    iconsContainer.append(timeContainer);
+    timeContainer.append(alarmIcon);
+    timeContainer.append(recipeTime);
+    iconsContainer.append(veganContainer);
+    iconsContainer.append(glutenContainer);
+    modalText.append(instructionText);
 
     const body = document.querySelector("body");
     body.addEventListener("click", (event) => {

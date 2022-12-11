ea1ece2086e6e525bb9474fc5823f9816079bcfe
ROXBOZ <hello@roxanne-borloz.net>
2022-12-11 23:28:08 +0100

    reorganisation functions

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,6 +1,7 @@
 /* EDAMAM Grid */
 
 // 1. fetch the API with AJAX
+
 const url =
   "https://api.edamam.com/api/recipes/v2?app_key=e51bcbcbffcb38a9c409dcf3eefbda24&app_id=c2bb6cfc&q=soup&type=public";
 
@@ -19,39 +20,39 @@ const soups = () => {
       console.log("error: ", error);
     });
 };
-soups();
 
-// 2. DOM manipulation
+// 2. Build Grid
+
 function buildGrid(soups) {
   const container = document.getElementById("recipe-page-container");
   container.setAttribute("class", "container-xxl");
+
   //rows
   const row = document.createElement("div");
   row.setAttribute("class", "row");
   row.style.maxWidth = "100vw";
   container.append(row);
 
+  // cols
   for (i = 0; i < soups.length; i++) {
-    // col-grid
-    const greatContainer = document.createElement("div");
-    greatContainer.setAttribute(
+    const cardContainer = document.createElement("div");
+    cardContainer.setAttribute(
       "class",
       "pb-4 grid-container col-xs-12 col-md-6 col-lg-4"
     );
-
-    row.append(greatContainer);
+    cardContainer.setAttribute("id", "cardContainer");
+    row.append(cardContainer);
 
     // cards
     const card = document.createElement("div");
     card.setAttribute("class", "card border-primary h-100");
     card.style.overflow = "hidden";
-    greatContainer.append(card);
+    cardContainer.append(card);
 
     // card images
     const cardImg = document.createElement("img");
     const recipeImg = soups[i]["recipe"]["image"];
     cardImg.src = recipeImg;
-
     cardImg.alt = soups[i]["title"];
     card.append(cardImg);
 
@@ -62,129 +63,117 @@ function buildGrid(soups) {
 
     // card title
     const cardTitle = document.createElement("h2");
-    cardTitle.innerText = soups[i]["recipe"]["label"]; //replacement
+    cardTitle.innerText = soups[i]["recipe"]["label"];
     cardBody.append(cardTitle);
 
     // card Button
     const cardButton = document.createElement("button");
     cardButton.setAttribute("id", "recipeOpen");
+    cardButton.setAttribute("value", i);
     cardButton.setAttribute("class", "btn btn-primary");
     cardButton.innerText = "Recipe";
     cardBody.append(cardButton);
-    cardButton.addEventListener("click", () => {
-      cardModal.style.display = "block";
-    });
+  }
+  recipeButton(soups);
+}
+soups();
 
-    // modal inc. background
-    const cardModal = document.createElement("div");
-    cardModal.setAttribute("id", "cardModal");
-    cardModal.setAttribute("class", "modal p-xs-0 p-md-5 mx-auto");
+// Recipe Button
+
+function recipeButton(soups) {
+  const recipieButtons = document.querySelectorAll("#recipeOpen");
+  recipieButtons.forEach((recipieButton) => {
+    recipieButton.addEventListener("click", (e) => {
+      const clickedSoupId = e.target.value;
+      createModal(soups, clickedSoupId);
+      modalIngredients(soups);
+    });
+  });
+}
+
+// Create Modal
+
+function createModal(soups, i) {
+  const cardModal = document.createElement("div");
+  const cardContainer = document.getElementById("cardContainer");
+  cardModal.setAttribute("id", "cardModal");
+  cardModal.setAttribute("class", "modal p-xs-0 p-md-5 mx-auto");
+  cardContainer.append(cardModal);
+  cardModal.style.display = "block";
+  const modalContent = document.createElement("div");
+  modalContent.setAttribute(
+    "class",
+    "modal-content bg-white h-xs-100 h-md-auto w-xs-100 w-md-75 p-2 border-primary mx-auto"
+  );
+  cardModal.append(modalContent);
+
+  //  modal close button
+  const modalClose = document.createElement("i");
+  modalClose.setAttribute("class", "close text-primary fa-solid fa-xmark m-3");
+  modalContent.append(modalClose);
+  modalClose.addEventListener("click", () => {
     cardModal.style.display = "none";
-    cardModal.style.position = "fixed";
-    cardModal.style.zIndex = "1";
-    cardModal.style.overflow = "auto";
-    cardModal.style.width = "100%";
-    cardModal.style.height = "100%";
-    cardModal.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    greatContainer.append(cardModal);
+  });
 
-    // modal box
-    const modalContent = document.createElement("div");
-    modalContent.setAttribute(
-      "class",
-      "modal-content bg-white h-xs-100 h-md-auto w-xs-100 w-md-75 p-2 border-primary mx-auto"
-    );
-    cardModal.append(modalContent);
+  //modal content container
+  const modalText = document.createElement("div");
+  modalText.setAttribute("class", "container recipe px-5 pb-5");
+  modalContent.append(modalText);
 
-    //  modal close button
-    const modalClose = document.createElement("i");
-    modalClose.setAttribute(
-      "class",
-      "close text-primary fa-solid fa-xmark m-3"
-    );
-    modalClose.style.textAlign = "right";
-    modalClose.style.fontSize = "200%";
-    modalClose.style.justifySelf = "flex-end";
-    modalClose.style.fontWeight = "bold";
-    modalContent.append(modalClose);
-    modalClose.addEventListener("click", () => {
-      cardModal.style.display = "none";
-    });
+  //title
+  const modalTitle = document.createElement("h2");
+  modalTitle.setAttribute("class", "mb-5");
+  modalTitle.innerText = soups[i]["recipe"]["label"];
+  modalText.append(modalTitle);
 
-    //modal content container
-    const modalText = document.createElement("div");
-    modalText.setAttribute("class", "container recipe px-5 pb-5");
-    modalContent.append(modalText);
+  //row
+  const modalRow = document.createElement("div");
+  modalRow.setAttribute("class", "row");
+  modalText.append(modalRow);
 
-    //title
-    const modalTitle = document.createElement("h2");
-    modalTitle.setAttribute("class", "mb-5");
-    modalTitle.innerText = soups[i]["recipe"]["label"]; //replacement
-    modalText.append(modalTitle);
+  // col 1 = ingredients
+  const ingredientsContainer = document.createElement("div");
+  ingredientsContainer.setAttribute("class", "ingredients col-md-6");
+  const ingUl = document.createElement("ul");
+  ingUl.setAttribute("id", "ingList");
+  ingUl.style.maxWidth = "45ch";
+  modalRow.append(ingredientsContainer);
+  ingredientsContainer.append(ingUl);
 
-    //row
-    const modalRow = document.createElement("div");
-    modalRow.setAttribute("class", "row");
-    modalText.append(modalRow);
+  //col 2
+  const secondCol = document.createElement("div");
+  secondCol.setAttribute("class", "col-md-6");
+  modalRow.append(secondCol);
 
-    // col 1 = ingredients
-    const recipeIng = document.createElement("div");
-    recipeIng.setAttribute("class", "ingredients col-md-6");
-    const ingList = document.createElement("ul");
-    const extIng = soups[i]["recipe"]["ingredientLines"];
-    modalRow.append(recipeIng);
+  // icons container
+  const iconsContainer = document.createElement("p");
+  iconsContainer.setAttribute("class", "icons-container");
+  secondCol.append(iconsContainer);
 
-    for (j = 0; j < extIng.length; j++) {
-      const ingName = extIng[j];
-      const ingListItem = document.createElement("li");
-      ingListItem.innerText = ingName;
-      ingList.append(ingListItem);
-    }
-    recipeIng.append(ingList);
+  // time
+  const recipeTime = soups[i]["recipe"]["totalTime"];
+  if (recipeTime !== 0) {
+    const timeContainer = document.createElement("span");
+    timeContainer.setAttribute("class", "icon-container mr-5");
+    const alarmIcon = document.createElement("i");
+    alarmIcon.setAttribute("class", "fa-solid fa-bell m-1");
+    const timeContainerText = `${recipeTime} min.`;
+    timeContainer.append(alarmIcon);
+    timeContainer.append(timeContainerText);
+    iconsContainer.append(timeContainer);
+  }
 
-    //col 2
-    const secondCol = document.createElement("div");
-    secondCol.setAttribute("class", "col-md-6");
-    modalRow.append(secondCol);
-
-    // icons container
-    const iconsContainer = document.createElement("p");
-    iconsContainer.setAttribute("class", "icons-container");
-    secondCol.append(iconsContainer);
-
-    // time
-    const recipeTime = soups[i]["recipe"]["totalTime"];
-    if (recipeTime !== 0) {
-      const timeContainer = document.createElement("span");
-      timeContainer.setAttribute("class", "icon-container mr-5");
-      const alarmIcon = document.createElement("i");
-      alarmIcon.setAttribute("class", "fa-solid fa-bell m-1");
-      const timeContainerText = `${recipeTime} min.`;
-      timeContainer.append(alarmIcon);
-      timeContainer.append(timeContainerText);
-      iconsContainer.append(timeContainer);
-    }
-
-    // vegetarian
-    const vegetarianContainer = document.createElement("span");
-    const isItVegetarian = soups[i]["recipe"]["healthLabels"];
-    vegetarianContainer.setAttribute("class", "icon-container");
-    if (isItVegetarian.includes("Vegetarian")) {
-      const itIsVegetarian = document.createElement("i");
-      itIsVegetarian.setAttribute("class", "fa-solid fa-carrot m-1");
-      const itIsVegetarianText = "vegetarian";
-      vegetarianContainer.append(itIsVegetarian);
-      vegetarianContainer.append(itIsVegetarianText);
-    } else {
-      const itIsNotVegetarian = document.createElement("i");
-      itIsNotVegetarian.setAttribute("class", "fa-solid fa-drumstick-bite m-1");
-      const itIsNotVegetarianText = "meaty";
-      vegetarianContainer.append(itIsNotVegetarian);
-      vegetarianContainer.append(itIsNotVegetarianText);
-    }
-    iconsContainer.append(vegetarianContainer);
-
-    // vegan
+  // vegetarian
+  const vegetarianContainer = document.createElement("span");
+  const isItVegetarian = soups[i]["recipe"]["healthLabels"];
+  vegetarianContainer.setAttribute("class", "icon-container");
+  if (isItVegetarian.includes("Vegetarian")) {
+    const itIsVegetarian = document.createElement("i");
+    itIsVegetarian.setAttribute("class", "fa-solid fa-carrot m-1");
+    const itIsVegetarianText = "vegetarian";
+    vegetarianContainer.append(itIsVegetarian);
+    vegetarianContainer.append(itIsVegetarianText);
+    //vegan
     const veganContainer = document.createElement("span");
     const isItVegan = soups[i]["recipe"]["healthLabels"];
     veganContainer.setAttribute("class", "icon-container");
@@ -194,58 +183,70 @@ function buildGrid(soups) {
       const itIsVeganText = "vegan";
       veganContainer.append(itIsVegan);
       veganContainer.append(itIsVeganText);
-    } else {
-      const itIsNotVegan = document.createElement("i");
-      itIsNotVegan.setAttribute("class", "fa-solid fa-skull m-1");
-      const itIsNotVeganText = "not vegan";
-      veganContainer.append(itIsNotVegan);
-      veganContainer.append(itIsNotVeganText);
     }
     iconsContainer.append(veganContainer);
+  } else {
+    const itIsNotVegetarian = document.createElement("i");
+    itIsNotVegetarian.setAttribute("class", "fa-solid fa-drumstick-bite m-1");
+    const itIsNotVegetarianText = "meaty";
+    vegetarianContainer.append(itIsNotVegetarian);
+    vegetarianContainer.append(itIsNotVegetarianText);
+  }
+  iconsContainer.append(vegetarianContainer);
 
-    //gluten
-    const glutenContainer = document.createElement("span");
-    const isItglutenFree = soups[i]["recipe"]["healthLabels"];
-    if (isItglutenFree.includes("Gluten-Free")) {
-      const itIsglutenFree = document.createElement("i");
-      itIsglutenFree.setAttribute("class", "fa-solid fa-bowl-rice m-1");
-      const itIsglutenFreeText = "gluten free";
-      glutenContainer.append(itIsglutenFree);
-      glutenContainer.append(itIsglutenFreeText);
-    } else {
-      const itIsNotglutenFree = document.createElement("i");
-      itIsNotglutenFree.setAttribute("class", "fa-solid fa-wheat-awn m-1");
-      const itIsNotglutenFreeText = "contains gluten";
-      glutenContainer.append(itIsNotglutenFree);
-      glutenContainer.append(itIsNotglutenFreeText);
+  //gluten
+  const glutenContainer = document.createElement("span");
+  const isItglutenFree = soups[i]["recipe"]["healthLabels"];
+  if (isItglutenFree.includes("Gluten-Free")) {
+    const itIsglutenFree = document.createElement("i");
+    itIsglutenFree.setAttribute("class", "fa-solid fa-bowl-rice m-1");
+    const itIsglutenFreeText = "gluten free";
+    glutenContainer.append(itIsglutenFree);
+    glutenContainer.append(itIsglutenFreeText);
+  } else {
+    const itIsNotglutenFree = document.createElement("i");
+    itIsNotglutenFree.setAttribute("class", "fa-solid fa-wheat-awn m-1");
+    const itIsNotglutenFreeText = "contains gluten";
+    glutenContainer.append(itIsNotglutenFree);
+    glutenContainer.append(itIsNotglutenFreeText);
+  }
+  iconsContainer.append(glutenContainer);
+
+  //redirection blog
+  const instructionText = document.createElement("p");
+  const instructionLink = document.createElement("a");
+  instructionLink.style.fontWeight = "bold";
+  const redirectionIcon = document.createElement("i");
+  redirectionIcon.setAttribute("class", "fa-solid fa-diamond-turn-right");
+  const instructionUrl = soups[i]["recipe"]["url"];
+  const blogName = soups[i]["recipe"]["source"];
+  instructionLink.href = instructionUrl;
+  instructionLink.innerText = ` Instruction on ${blogName}`;
+  instructionText.append(redirectionIcon);
+  instructionText.append(instructionLink);
+  secondCol.append(instructionText);
+
+  /* quit the modal when clicking outside */
+  const body = document.querySelector("body");
+  body.addEventListener("click", (event) => {
+    if (event.target == cardModal) {
+      cardModal.style.display = "none";
     }
-    iconsContainer.append(glutenContainer);
+  });
+}
 
-    //instructions
-    const instructionText = document.createElement("p");
-    const instructionLink = document.createElement("a");
-    instructionLink.style.fontWeight = "bold";
-    const redirectionIcon = document.createElement("i");
-    redirectionIcon.setAttribute("class", "fa-solid fa-diamond-turn-right");
-    const instructionUrl = soups[i]["recipe"]["url"];
-    const blogName = soups[i]["recipe"]["source"];
-    instructionLink.href = instructionUrl;
-    instructionLink.innerText = ` Instruction on ${blogName}`;
-    instructionText.append(redirectionIcon);
-    instructionText.append(instructionLink);
-    secondCol.append(instructionText);
-
-    /* quit the modal when clicking outside */
-    const body = document.querySelector("body");
-    body.addEventListener("click", (event) => {
-      if (event.target == cardModal) {
-        cardModal.style.display = "none";
-      }
-    });
+function modalIngredients(soups) {
+  const ingUl = document.querySelector(".ingredients");
+  const ingredientLi = soups[i]["recipe"]["ingredientLines"][j];
+  for (j = 0; j < ingredientLi.length; j++) {
+    const ingName = ingredientLi[j];
+    const ingListItem = document.createElement("li");
+    ingListItem.innerText = ingName;
+    ingUl.append(ingListItem);
   }
 }
 
-/* show more button */
+/* show more button on home page */
 function showMore() {
   let dots = document.getElementById("dots");
   let moreText = document.getElementById("more");
